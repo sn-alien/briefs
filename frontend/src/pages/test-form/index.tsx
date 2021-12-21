361177044773a6d2c24c457d80da3b3636c1e2d8
Nazar-Klymenko <52259266+Nazar-Klymenko@users.noreply.github.com>
2021-12-21 21:46:02 +0100

    feat: added rhf and yup

---
+++ frontend/src/pages/test-form/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import * as yup from 'yup';
+
+type Inputs = {
+  example: string;
+  exampleRequired: string;
+};
+
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input defaultValue="test" {...register('example')} />
+
+      <input {...register('exampleRequired', { required: true })} />
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      <input type="submit" />
+    </form>
+  );
+}
