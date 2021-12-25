9272cbd5e900386f63a5cd33cc49512f17322caf
Nazar-Klymenko <72665140+sofia-mikhalova@users.noreply.github.com>
2021-12-25 17:13:44 +0100

    style: theme color

--- frontend/src/styles/muiTheme.ts
+++ frontend/src/styles/muiTheme.ts
@@ -11,14 +11,14 @@ const themeName = "Light";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#46D5B3",
-      light: "#98E8D4",
-      dark: "#31D0AA",
-    },
-    secondary: {
       main: "#EF5DA8",
       light: "#F17BB8",
       dark: "#ED4B9E",
+    },
+    secondary: {
+      main: "#46D5B3",
+      light: "#98E8D4",
+      dark: "#31D0AA",
     },
   },
   themeName,
